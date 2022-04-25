ce4acd1bd92ef604e35074d40bac6bfe732994db
Anirudh Rao <anirudh131299@gmail.com>
2022-04-25 11:33:34 +0530

    Some Tinckering

--- pages/index.js
+++ pages/index.js
@@ -5,7 +5,7 @@ import styles from '../styles/Home.module.css'
 export default function Home() {
   return (
     <div className={styles.container}>
-      <h1 className="text-3xl font-bold underline">
+      <h1 className="text-3xl font-bold underline flex-wrap">
       Hello world!
     </h1>
     </div>
